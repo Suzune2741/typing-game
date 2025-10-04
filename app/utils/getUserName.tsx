1fcd467cbdb1de35731f692d6418e0fa61a12c28
Suzune2741 <kannra1174@gmail.com>
2025-10-04 21:05:51 +0900

    fix: リロードしたらエラーが出ていたのを修正

--- app/utils/getUserName.tsx
+++ app/utils/getUserName.tsx
@@ -1,5 +1,7 @@
 export const getUserName = (needData: string, defaultValue: string) => {
-  const userName = localStorage.getItem(needData);
-  if (!userName) return defaultValue;
-  return userName !== null ? userName : defaultValue;
+  if (typeof window !== "undefined") {
+    const userName = localStorage.getItem(needData);
+    if (!userName) return defaultValue;
+    return userName !== null ? userName : defaultValue;
+  }
 };
