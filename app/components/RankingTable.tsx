1bcc0746b86b727e98eb16129b2d39efcc7d133b
Suzune2741 <kannra1174@gmail.com>
2025-10-23 16:32:36 +0900

    fix: ランキング表示の際に表示するデータを変えるようにした

--- app/components/RankingTable.tsx
+++ app/components/RankingTable.tsx
@@ -25,28 +25,48 @@ export const RankingTable = ({
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            {normalModeHead.map((element, index) => (
-              <th
-                key={index}
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {element}
-              </th>
-            ))}
+            {gameMode === "normal"
+              ? normalModeHead.map((element, index) => (
+                  <th
+                    key={index}
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {element}
+                  </th>
+                ))
+              : timeAttackModeHead.map((element, index) => (
+                  <th
+                    key={index}
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {element}
+                  </th>
+                ))}
           </tr>
         </thead>
         <tbody>
-          {rankingData.map((element, index) => (
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 text-gray-900 whitespace-nowrap dark:text-white">
-              <th className=" px-6 py-4">{index + 1}</th>
-              <th key={index} className="px-6 py-4 ">
-                {element.name}
-              </th>
-              <td className="px-6 py-4">{element.endTime}</td>
-              <td className="px-6 py-4">{element.missType}</td>
-              <td className="px-6 py-4">{element.difficulty}</td>
-            </tr>
-          ))}
+          {gameMode === "normal"
+            ? rankingData.map((element, index) => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 text-gray-900 whitespace-nowrap dark:text-white">
+                  <th className=" px-6 py-4">{index + 1}</th>
+                  <th key={index} className="px-6 py-4 ">
+                    {element.name}
+                  </th>
+                  <td className="px-6 py-4">{element.endTime}</td>
+                  <td className="px-6 py-4">{element.missType}</td>
+                  <td className="px-6 py-4">{element.difficulty}</td>
+                </tr>
+              ))
+            : rankingData.map((element, index) => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 text-gray-900 whitespace-nowrap dark:text-white">
+                  <th className=" px-6 py-4">{index + 1}</th>
+                  <th key={index} className="px-6 py-4 ">
+                    {element.name}
+                  </th>
+                  <td className="px-6 py-4">{element.score}</td>
+                  <td className="px-6 py-4">{element.missType}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
