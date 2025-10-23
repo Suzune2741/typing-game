ab0df3616c4a1988316ab68bcff0889abd6bb77c
Suzune2741 <kannra1174@gmail.com>
2025-10-23 18:02:10 +0900

    feat; deploy setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
+import { getLoadContext } from "./load-context";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    cloudflareDevProxy({ getLoadContext }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
   build: {
     target: "esnext",
   },
