2f3ad2984e455d42f0c5c47aeb0cf84ac4cc8735
Suzune2741 <kannra1174@gmail.com>
2025-10-28 11:29:02 +0900

    fix: デプロイの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
-import { getLoadContext } from "./load-context";
 
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
-    cloudflareDevProxy({ getLoadContext }),
     reactRouter(),
     tsconfigPaths(),
   ],
-  build: {
-    target: "esnext",
-  },
 });
